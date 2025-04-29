f2c7f99bc4282f2aaff75cc7f484f269faecaff7
vishalidinesh1998 <vishalidinesh1998@gmail.com>
2025-04-29 10:48:09 +0530

    updated the Files

---
+++ script.js
@@ -0,0 +1,33 @@
+function generatecaptchaText(length=6) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let captcha = '';
+ for(i=0;i<length;i=i+1){
+    captcha +=characters.charAt(Math.floor(Math.random() * characters.length))
+ }
+ return captcha;
+ 
+}
+
+let gcaptcha=document.getElementById("captcha")
+gcaptcha.textContent=generatecaptchaText()
+let verify=document.getElementById("verify")
+form=document.getElementById("form")
+verify.addEventListener("click",function(){
+   let userinput=document.getElementById("user").value
+    if(userinput===""){
+        alert("Fill the Captcha")
+    }
+
+   else if(userinput=== gcaptcha.textContent){
+        alert("Matched")
+        form.submit();
+    }
+    else{
+        alert("not Matched")
+         form.submit();
+    }
+
+})
+
+
+    
